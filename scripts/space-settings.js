b53dca8ee13dc76c1ce10838abd50cdd0b72cb04
Neopentene <82939252+Neopentene@users.noreply.github.com>
2022-10-11 21:53:25 +0530

    initial commit

--- scripts/space-settings.js
+++ scripts/space-settings.js
@@ -9,15 +9,15 @@ function generateBody() {
         <div class="d-flex a-f-cen f-col">
             <label for="star-count-range">Star Count</label>
             <input class="min-w-224" type="range" name="star-count-range" id="star-count-range" min="500" max="10000">
-            <input class="input simple text-cen" type="number" id="star-count-value"></input>
+            <input class="input simple text-cen" type="number" id="star-count-value" data-label="stars"></input>
             <br>
             <label for="star-speed-range">Star Speed</label>
             <input class="min-w-224" type="range" name="star-speed-range" id="star-speed-range" min="1" max="1000">
-            <input class="input simple text-cen" type="number" step="0.01" id="star-speed-value" min="0.01"></input>
+            <input class="input simple text-cen" type="number" step="0.001" id="star-speed-value" min="0.01" data-label="starSpeed"></input>
             <br>
             <label for="star-rotation-range">Rotation Speed</label>
             <input class="min-w-224" type="range" name="star-rotation-range" id="star-rotation-range" min="1" max="1000">
-            <input class="input simple text-cen" type="number" step="0.01" id="star-rotation-value" min="0.01"></input>
+            <input class="input simple text-cen" type="number" step="0.001" id="star-rotation-value" min="0.01" data-label="starRotation"></input>
         </div>
     `
 }
@@ -32,59 +32,51 @@ function generateFooter() {
 
 document.getElementById('space-settings').addEventListener('click', () => {
     openModal("modal-space-settings", generateHeader(), generateBody(), generateFooter(), true);
-    const starCountRange = document.getElementById('star-count-range');
-    const starSpeedRange = document.getElementById('star-speed-range');
-    const starRotationRange = document.getElementById('star-rotation-range');
-    const starCountRangePara = document.getElementById('star-count-value');
-    const starSpeedRangePara = document.getElementById('star-speed-value');
-    const starRotationRangePara = document.getElementById('star-rotation-value');
 
-    starCountRangePara.value = localStorage.getItem('stars');
-    starSpeedRangePara.value = localStorage.getItem('starSpeed');
-    starRotationRangePara.value = localStorage.getItem('starRotation');
+    const ranges = [
+        document.getElementById('star-count-range'),
+        document.getElementById('star-speed-range'),
+        document.getElementById('star-rotation-range'),
+    ]
 
-    starCountRange.value = localStorage.getItem('stars');
-    starSpeedRange.value = Number.parseFloat(localStorage.getItem('starSpeed')) * 1000;
-    starRotationRange.value = Number.parseFloat(localStorage.getItem('starRotation')) * 1000;
+    const rangeValues = [
+        document.getElementById('star-count-value'),
+        document.getElementById('star-speed-value'),
+        document.getElementById('star-rotation-value'),
+    ]
 
-    starCountRangePara.addEventListener('input', () => {
-        starCountRange.value = clamp(Number.parseInt(starCountRangePara.value), 500, 10000);
-    })
+    const rangeBoundaries = [
+        { min: 500, max: 10000, scale: 1 },
+        { min: 0.01, max: 1, scale: 1000 },
+        { min: 0.01, max: 1, scale: 1000 },
+    ]
 
-    starSpeedRangePara.addEventListener('input', () => {
-        starSpeedRange.value = clamp(Number.parseFloat(starSpeedRangePara.value) * 1000, 1, 1000);
-    })
+    rangeValues.forEach((range, index) => {
+        const baseValue = Number.parseFloat(localStorage.getItem(range.getAttribute('data-label')));
+        range.value = baseValue;
+        ranges[index].value = Number.parseInt(baseValue * rangeBoundaries[index].scale);
 
-    starRotationRangePara.addEventListener('input', () => {
-        starRotationRange.value = clamp(Number.parseFloat(starRotationRangePara.value) * 1000, 1, 1000);
-    })
+        range.addEventListener('input', () => {
+            let value = Number.parseInt(range.value) * rangeBoundaries[index].scale;
+            ranges[index].value = clamp(value, rangeBoundaries[index].min, rangeBoundaries[index].max);
+        })
 
-    starCountRange.addEventListener('input', (event) => {
-        let value = clamp(starCountRange.value, 500, 10000);
-        starCountRangePara.value = value;
-    });
-
-    starSpeedRange.addEventListener('input', () => {
-        let value = clamp(starSpeedRange.value / 1000, 0.01, 1);
-        starSpeedRangePara.value = value;
-    });
-
-    starRotationRange.addEventListener('input', () => {
-        let value = clamp(starRotationRange.value / 1000, 0.01, 1);
-        starRotationRangePara.value = value;
+        ranges[index].addEventListener('input', () => {
+            let value = ranges[index].value / rangeBoundaries[index].scale;
+            range.value = clamp(value, rangeBoundaries[index].min, rangeBoundaries[index].max);
+        })
     });
 
     document.getElementById('settings-save').addEventListener('click', () => {
         const event = new Event('reRender');
-        const count = starCountRangePara.value? Number.parseInt(starCountRangePara.value) : 0;
-        const speed = starSpeedRangePara.value? Number.parseInt(starSpeedRangePara.value) : 0;
-        const rotation = starRotationRangePara? Number.parseInt(starRotationRangePara.value) : 0;
 
-        localStorage.setItem('stars', clamp(count, 500, 10000));
-        localStorage.setItem('starSpeed', clamp(speed, 0.01, 1));
-        localStorage.setItem('starRotation', clamp(rotation, 0.01, 1));
+        rangeValues.forEach((range, index) => {
+            let value = index? Number.parseFloat(range.value) : Number.parseInt(range.value);
+            value = clamp(value, rangeBoundaries[index].min, rangeBoundaries[index].max);
+            localStorage.setItem(range.getAttribute('data-label'), value);
+        });
 
         window.dispatchEvent(event);
-        // closeModal(document.getElementById("modal-space-settings"));
+        closeModal(document.getElementById("modal-space-settings"));
     });
 })
